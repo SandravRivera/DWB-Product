69b8eb54414d406c3a0b56cd373e04f5ed655fe2
Sandra Rivera <sandrarivera@ciencias.unam.mx>
2026-03-02 20:25:49 -0600

    feat: Endpoint para mostrar objetos Category
    Se insertó la clase Category en product y se generó un endpoint para mostrar un ejemplo de arreglo de clases.

---
+++ product/src/main/java/com/product/Category.java
@@ -0,0 +1,61 @@
+package com.product;
+
+public class Category {
+    private Integer category_id;
+    private String category;
+    private String tag;
+    private Integer status;
+
+    public Category(Integer category_id, String category, String tag, Integer status) {
+        this.category_id = category_id;
+        this.category = category;
+        this.tag = tag;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + category_id + ", '" + category + "', '" + tag + "', " + status + "}";
+    }
+
+    /******************
+     * SETTERS
+     ******************/
+    
+    public void setCategory_id(Integer category_id) {
+        this.category_id = category_id;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    /******************
+     * GETTERS
+     ******************/
+
+    public Integer getCategory_id() {
+        return category_id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+}
