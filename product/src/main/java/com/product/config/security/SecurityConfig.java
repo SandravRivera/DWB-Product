0d6de35f2232a36f4ad32a68afa558113e3083c4
Sandra Rivera <sandrarivera@ciencias.unam.mx>
2026-04-25 12:50:29 -0600

    feat: Autentificación para acceder a endpoints
    Ahora los usuarios tienen que iniciar sesión en API Auth para poder acceder a los servicios. Los administradores podrán acceder a todos los endpoints, mientras los clientes solo podrán ver categorías, detalles de un producto y sus imágenes.

---
+++ product/src/main/java/com/product/config/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.product.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.product.config.jwt.JwtAuthFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtFilter;
+
+    // Cambiar si los roles tienen otro nombre
+    String ADMIN = "Administrator", COSTUMER = "User";
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfig corsConfig) throws Exception {
+    
+        http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+                auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/error", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/info", "/actuator/health").permitAll()
+                // El cliente puede ver las categorías activas, los detalles de un producto y sus imágenes
+				.requestMatchers(HttpMethod.GET, "/category/active").hasAnyAuthority(ADMIN, COSTUMER)
+				.requestMatchers(HttpMethod.GET, "/product/{id}/**").hasAnyAuthority(ADMIN, COSTUMER)
+				// El administrador tiene permisos para todo
+				.requestMatchers("/category/**").hasAuthority(ADMIN)
+				.requestMatchers("/product/**").hasAuthority(ADMIN)
+                .anyRequest().authenticated()
+                )
+        .cors(cors -> cors.configurationSource(corsConfig))
+        .httpBasic(Customizer.withDefaults())
+        .formLogin(form -> form.disable())
+        .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            
+        return http.build();
+    }
+}
